274366aa43795b11f5227e219778de8fbb29a610
unknown <jefersonmallone2000@outlook.com>
2021-07-08 23:13:37 -0300

    first commit

---
+++ resources/views/auth/forgot_password.blade.php
@@ -0,0 +1,40 @@
+@extends('layout.master')
+<div id="auth">
+    <div class="row h-100">
+        <div class="col-lg-5 col-12">
+            <div id="auth-left">
+                <h3 class="auth-title"> Esqueci minha senha </h3>
+                <p class="auth-subtitle mb-5"> 
+                    Informe seu e-mail e enivaremos um link de recuperação
+                </p>
+
+                <form> 
+                    <div class="form-group position-relative has-icon-left mb-4">
+                        <input 
+                            type="email" 
+                            class="form-control form-control-x1"
+                            placeholder="Digite seu e-mail aqui"
+                        />
+                        <div class="form-control-icon">
+                            <i class="bi bi-envelope"> </i>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">
+                        Enviar
+                    </button>
+                </form>
+
+                <div class="text-center mt-5 text-lg fs-4">
+                    <p class="text-gray-600">
+                         Lembra da sua conta? 
+                        <a href="/" class="font-bold"> Fazer Login </a>
+                    </p>
+                </div>  
+            </div>
+        </div>
+        <div class="col-lg-7 d-none d-lg-block">
+            <div id="auth-right"> </div>
+        </div>
+    </div>
+</div>
+
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title> Jefin Finances </title>
+    <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+        crossorigin="anonymous"
+    >
+
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/mazer-theme/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/mazer-theme/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/mazer-theme/pages/auth.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/mazer-theme/pages/chat.css') }} ">
+    <link rel="stylesheet" href="{{ asset('css/mazer-theme/pages/dripicons.css') }} ">
+    <link rel="stylesheet" href="{{ asset('css/mazer-theme/pages/email.css') }} ">
+    <link rel="stylesheet" href="{{ asset('css/mazer-theme/pages/error.css') }} ">
+    <link rel="stylesheet" href="{{ asset('css/mazer-theme/vendors/bootstrap-icons/bootstrap-icons.css') }}">
+    
+    <link rel="stylesheet" href="{{ asset('css/app.css') }} "/>   
+</head>
+<body style="background-color:#e7e7e7">
+    @yield('content')
+    <script 
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
+        crossorigin="anonymous"
+    ></script>
+    <script src=""> </script>
+    <script src=""> </script>
+    <script src=""> </script>
+    <script src=""> </script>
+    <script src="{{ asset('js/app.js') }}"> </script>
+</body>
+</html>
