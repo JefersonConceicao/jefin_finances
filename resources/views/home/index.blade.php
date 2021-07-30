9fe114eca02ed1f3cd1b283bcd0660f940a93c9f
unknown <jefersonmallone2000@outlook.com>
2021-07-29 22:42:17 -0300

    no message

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.page')
 @section('content_header')
-    <h3> Início </h3>
+    <h3> Oi {{ Auth::user()->name }} </h3>
+    <p class="text-subtitle"> Estamos desenvolvendo um dashboard para você, aguarde atualizações... </p>
 @endsection
 
 @section('content-title')
