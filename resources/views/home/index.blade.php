45b72930b5af27e4d1fd3a8b2403d5e1a45ebd64
Jeferson Conceicao <jefersonmallone2000@outlook.com>
2021-08-03 16:54:44 -0300

    dash user

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -70,11 +70,9 @@
                         <h4> Diferença de gastos dos ultimos meses </h4>
                     </div>
                     <div class="card-body">
-                        <div class="row">
-                            
-                        {{-- GRAFICO COMPARATIVO DE GASTOS POR MÊS  --}}
+                            {{-- GRAFICO COMPARATIVO DE GASTOS POR MÊS  --}}
                             <canvas id="myChart" width="400" height="150" class="chartjs-render-monitor"> </canvas>  
-                        {{-- FIM DO GRÁFICO --}}
+                            {{-- FIM DO GRÁFICO --}}
                     </div>
                 </div>
             </div>
