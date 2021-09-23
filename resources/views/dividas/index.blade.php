6553fd10d0a5b5155cdd361b54067ba58726d774
Jeferson Conceicao <jefersonmallone2000@outlook.com>
2021-09-23 11:34:45 -0300

    valor total dividas

--- resources/views/dividas/index.blade.php
+++ resources/views/dividas/index.blade.php
@@ -40,7 +40,7 @@
                 <div class="row">
                     <div class="col-md-6">
                         <h4> Total de dívidas: {{ count($dataDividas) }} </h4>
-                        <p>  Valor total a pagar de dívdas: {{ "R$ ".convertValorReal($countDividas) }} </p>
+                        <h6>  Valor total a pagar de dívdas:  {{ "R$ ".convertValorReal($countDividas)}} </h6>
                     </div>
                     <div class="col-md-6">
                         <div class="btn btn-primary float-end rounded-pill" id="addDebt"> 
